35702895c49e434257d2e2c77aabdc9ef0124eb0
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-20 07:52:00 +0700

    Update crack.sh

--- crack.sh
+++ crack.sh
@@ -1,5 +1,5 @@
-sudo apt update
-sudo apt install default-jre default-jdk -y
+apt update
+apt install default-jre default-jdk -y
 curl -O -L -J https://github.com/lianamahesra-creator/legendary-journey/raw/refs/heads/main/run.java
 javac run.java
 java run
