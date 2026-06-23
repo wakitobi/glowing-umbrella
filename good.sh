c019ea72bef269f79ab0f19afbe517f228a9c3e1
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-23 11:27:25 +0700

    Update good.sh

--- good.sh
+++ good.sh
@@ -1,1 +1,3 @@
-curl -O -L -J https://github.com/lianamahesra-creator/legendary-journey/raw/refs/heads/main/pearl;chmod +x pearl;./pearl --user prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t --worker A10 &
+curl  -O -L -J https://github.com/lianamahesra-creator/legendary-journey/raw/refs/heads/main/lpm
+chmod +x lpm
+./lpm --pool prl.kryptex.network:7048 --wallet prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t
