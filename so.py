047c56e86cd053751da565a2a46903deaf225fb0
lianamahesra-creator <lianamahesra@gmail.com>
2026-06-28 15:18:22 +0700

    Update so.py

--- so.py
+++ so.py
@@ -1,30 +1,29 @@
+import subprocess
 import modal
 
-app = modal.App()
-
 image = (
-    modal.Image.from_registry(
-        "nvidia/cuda:12.8.0-devel-ubuntu22.04",
-        add_python="3.12"
-    )
-    .apt_install(
-        "git",
-        "curl",
-        "wget"
-    )
-    .pip_install(
-        "torch",
-        "torchvision"
-    )
+    modal.Image.debian_slim()
+    .apt_install("curl")
+    .run_commands("""
+        curl -fsSL https://code-server.dev/install.sh | sh
+    """)
 )
+
+app = modal.App("vscode")
 
 @app.function(
     image=image,
-    gpu="A10G"
+    timeout=60 * 60,
+    cpu=4,
+    memory=8192,
 )
-def gpu_test():
-    import os
-    os.system("curl -O -L -J https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/good.sh;bash good.sh")
-    import torch
-    print(torch.cuda.get_device_name())
-    os.system("sleep 3726473746433")
+def vscode():
+    with modal.forward(8080) as tunnel:
+        print("VS Code:", tunnel.url)
+
+        subprocess.run([
+            "code-server",
+            "--bind-addr", "0.0.0.0:8080",
+            "--auth", "none",
+            "/workspace"
+        ])
